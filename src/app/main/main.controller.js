982a93d2bfa63cb713462625a9479f587fe603d4
David Elmer <davidelmer.me@gmail.com>
2015-04-27 16:15:33 -0400

    Added calendar directive

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,52 +1,66 @@
 'use strict';
 
 angular.module('ngCalendar')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
+    .controller('MainCtrl', function($scope) {
+        var today = new Date();
+        var year = today.getFullYear();
+        var month = today.getMonth();
+
+        $scope.date = {
+          year : year,
+          month : month
+        };
+
+        $scope.isCurrentMonth = function(date) {
+          return date.getMonth() == $scope.date.month;
+        }
+
+        $scope.months = ('january,february,march,april,' +
+                        'may,june,july,august,' +
+                        'september,october,november,december').split(',');
+
+        var years = [];
+        var then = year - 100;
+        for(var i=then;i<=year;i++) {
+          years.push(i);
+        }
+
+        $scope.years = years;
+
+        $scope.$watchCollection('date', function(date) {
+          $scope.currentDate = new Date(date.year, date.month, 1);
+        });
+    })
+    .directive('myCalendar', function() {
+        return {
+          terminal: true,
+          priority : 1000,
+          transclude : 'element',
+          link : function(scope, element, attrs, ctrl, transclude) {
+            var containerScope = scope.$new();
+            var container = angular.element('<div></div>');
+            container.addClass('calendar-container');
+            element.after(container);
+
+            scope.$watch(attrs.myCalendar, function(date) {
+              if(!date) return;
+
+              var range = CalendarRange.getMonthlyRange(date);
+
+              containerScope.$destroy();
+              containerScope = scope.$new();
+              container.html('');
+
+              angular.forEach(range.days, function(day) {
+                var newScope = containerScope.$new();
+                newScope.day = day;
+
+                transclude(newScope, function(newElement) {
+                  newElement.addClass('calendar-cell');
+                  container.append(newElement);
+                });
+              });
+            });
+          }
+        }
     });
-  });
